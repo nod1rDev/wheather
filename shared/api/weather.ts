1044fd67734239c219a1310372529b449eaf2e07
nod1rDev <nodirbeknegmatov2008@gmail.com>
2025-08-06 15:06:57 +0500

    changed eng api key

--- shared/api/weather.ts
+++ shared/api/weather.ts
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const API_KEY = process.env.NEXT_PUBLIC_OPENWEATHER_API_KEY
+const API_KEY = process.env.OPENWEATHER_API_KEY
 const BASE_URL = "https://api.openweathermap.org/data/2.5"
 
 // API_KEY mavjudligini tekshirish endi har bir funksiya ichida amalga oshiriladi.
